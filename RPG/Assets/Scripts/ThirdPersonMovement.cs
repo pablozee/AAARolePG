aab2ab138ef9481a47c3a154e82ac98813fcff8d
Zeshan Rasul <zeshanrasul@gmail.com>
2022-05-18 10:29:04 +0100

    Run and Jump animations

--- RPG/Assets/Scripts/ThirdPersonMovement.cs
+++ RPG/Assets/Scripts/ThirdPersonMovement.cs
@@ -17,6 +17,7 @@ public class ThirdPersonMovement : MonoBehaviour
     [SerializeField] private Transform cam;
 
     private CharacterController controller;
+    private Animator anim;
     private Vector2 movementValue;
     private float jumpValue;
     private bool isGrounded;
@@ -28,10 +29,18 @@ public class ThirdPersonMovement : MonoBehaviour
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         if (canMove) Move();
 
         if (canJump) Jump();
@@ -55,6 +64,8 @@ public class ThirdPersonMovement : MonoBehaviour
         float vertical = movementValue.y;
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
+        anim.SetFloat("MoveMagnitude", direction.magnitude);
+
         if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
@@ -70,19 +81,13 @@ public class ThirdPersonMovement : MonoBehaviour
     {
         if (jumpValue == 1.0f && isGrounded)
         {
+            anim.SetTrigger("Jump");
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
     }
 
     private void SimulateGravity()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
-        if (isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
-
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
